506cf49f9a40f673839e50c1130f332e8a73b70a
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-11-14 14:57:58 -0500

    stripped out music refernces

--- tweetBot.py
+++ tweetBot.py
@@ -1,11 +1,10 @@
 # Twitter Bot
 # by Stefano C. Coronado
 import tweepy as tweet
-import tswift as ts
 import time
 
 def main():
-    
+
     try:
         keyfile = open("apikeys.txt", 'r')
 
@@ -17,91 +16,29 @@ def main():
         CONSUMER_SECRET = keys[1]
         ACCESS_KEY = keys[2]
         ACCESS_SECRET = keys[3]
-        
+
         keyfile.close()
+
     except:
         print("An Unknown Error has occured importing the API Keys")
         print("Make sure the API Keys are properly formatted into apikeys.txt")
         exit(1)
-        
-        
+
+
     auth = tweet.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
-    api = tweet.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-    
+    twitter = tweet.API(auth ,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
     while True:
-        print("\nRefreshing...")
-        newest_mention, all_tweets = get_latest_tweets(api)
-        #all_tweets = api.mentions_timeline()
-
-        reply_to_tweet(api, newest_mention, all_tweets)
-        time.sleep(60)
-
-def song_lookup(song_lyric):
-
-    song_lyric = song_lyric.split(' ', 1)[1]
+        #TODO more logic
+        time.sleep(20) #seconds
+        message = "Live from NPR News in Washington this is is @BotNeno"
+        print("sending a ping to the account")
+        twitter.update_status("pong") #send the word pong
 
 
-    print("Song Lyrics:" , song_lyric)
-
-    the_song = ts.Song.find_song(song_lyric)
-
-    if the_song != None:
-        #TODO find a better way to retrieve the artist
-
-        song_artist = "That song is called " + the_song.title + " by " + the_song.artist
-    else:
-        song_artist = "Sorry! I don't recognize those lyrics"
-
-    return song_artist
-
-
-
-def reply_to_tweet(api, newest_mention, all_tweets):
-
-    #load tweet ids into array
-
-    already_replied = []
-
-    for i in range(1,len(all_tweets)): # want everything except for the first tweet
-        already_replied.append(all_tweets[i])
-    try:
-
-
-        if newest_mention.id != already_replied[0].id:
-
-
-            if newest_mention.text == "@NenoSong ping":
-                api.update_status('@' + newest_mention.user.screen_name + " pong", newest_mention.id)
-                print("pong!")
-            else:
-                if "\n" in newest_mention.text: #if the newest_mention contains newlines
-                    print("@" + newest_mention.user.screen_name +  " Didn't tweet one line")
-
-                    api.update_status('@' + newest_mention.user.screen_name + " Please write one line without line breaks", newest_mention.id)
-                else:
-                    song_info = song_lookup(newest_mention.text)
-                    api.update_status('@' + newest_mention.user.screen_name + " " + song_info, newest_mention.id)
-                    print("Sent song info")
-        else:
-
-            print("Already responded to this tweet")
-    except tweet.error.TweepError:
-        print("Status is duplicate")
-
-
-def get_latest_tweets(api):
-    # try-except because mentions will return an empty list if there are no tweets to this account    
-    try:
-        mentions = api.mentions_timeline() #list of all tweets where NenoSong is mentioned
-        newest_mention = mentions[0]
-        print("Latest Tweet", newest_mention.id ,'-', newest_mention.text , "from" , newest_mention.user.screen_name)
-    except IndexError:
-        print("Tweet to this account before trying again")
-        exit(1)
-    return newest_mention, mentions #return the entire instance to for next func to reply referencing the id
 
 print("Bot is running")
 main()
