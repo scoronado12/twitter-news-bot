9ed090188665d453f2b24754ecc1a93ce200d4cb
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-11-16 15:44:28 -0500

    currently parsing tweets, looking to split entire feed into multiple tweets

--- tweetBot.py
+++ tweetBot.py
@@ -1,7 +1,10 @@
 # Twitter Bot
 # by Stefano C. Coronado
 import tweepy as tweet
+import feedparser as fp
 import time
+import datetime
+
 
 def main():
 
@@ -31,13 +34,31 @@ def main():
 
     twitter = tweet.API(auth ,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-    while True:
-        #TODO more logic
-        time.sleep(20) #seconds
-        message = "Live from NPR News in Washington this is is @BotNeno"
-        print("sending a ping to the account")
-        twitter.update_status("pong") #send the word pong
+    #while True:
+        #time.sleep(20) #seconds
+        #print("sending a ping to the account")
+        #twitter.update_status("pong") #send the word pong
+    print(formulate_npr_tweet(twitter)) 
 
+def formulate_npr_tweet(api):
+    #We will post the latest four headlines
+
+    current_date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M hrs")
+
+    message = current_date_time + " Live from NPR News in Washington\n\n"
+
+    npr_feed = fp.parse("https://www.npr.org/rss/rss.php?id=1001")
+
+    for i in range(3):
+        message += " - " + npr_feed.entries[i].title + "\n"
+
+    print("Length of tweet " + str(len(message)))
+
+    #TODO
+
+    return message
+
+    #TODO add in the other
 
 
 print("Bot is running")
