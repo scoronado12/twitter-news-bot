ff7d7883177bfae77f250da6a23e118a0014e71b
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-11-17 16:49:39 -0500

    Bot is moreless working - Need to optimize

--- tweetBot.py
+++ tweetBot.py
@@ -34,32 +34,63 @@ def main():
 
     twitter = tweet.API(auth ,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-    #while True:
-        #time.sleep(20) #seconds
-        #print("sending a ping to the account")
-        #twitter.update_status("pong") #send the word pong
-    print(formulate_npr_tweet(twitter)) 
+
+
+    while True:
+
+        formulate_npr_tweet(twitter)
+
+        time.sleep(3600) #one hour. Make sure you run this on the 30th minute or on the 00th minute 
+
+
+
 
 def formulate_npr_tweet(api):
     #We will post the latest four headlines
+    try:
+        current_date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M hrs")
 
-    current_date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M hrs")
+        message = current_date_time + " Live from NPR News in Washington (1/5)\n"
 
-    message = current_date_time + " Live from NPR News in Washington\n\n"
+        npr_feed = fp.parse("https://www.npr.org/rss/rss.php?id=1001")
+        #TODO optimize later 15 headlines will go out in a series of five tweets
 
-    npr_feed = fp.parse("https://www.npr.org/rss/rss.php?id=1001")
+        for i in range(0,3):
+            message += " - " + npr_feed.entries[i].title + "\n"
 
-    for i in range(3):
-        message += " - " + npr_feed.entries[i].title + "\n"
+        print(message)
+        api.update_status(message)
 
-    print("Length of tweet " + str(len(message)))
+        message = "(2/4)\n"
 
-    #TODO
+        for i in range(4,7):
+            message += " - " + npr_feed.entries[i].title + "\n"
 
-    return message
+        print(message)
+        api.update_status(message)
 
-    #TODO add in the other
+        message = "(3/4)\n"
 
+
+        for i in range(8,11):
+            message += " - " + npr_feed.entries[i].title + "\n"
+
+        print(message)
+        api.update_status(message)
+
+
+        message = "(4/4)\n"
+
+        for i in range(12,15):
+            message +=  " - " + npr_feed.entries[i].title + "\n"
+
+        #detect that the last dash has been printed
+        message += "END OF LINE\n For more details, please visit https://npr.org/"
+        print(message)
+
+        api.update_status(message)
+    except tweet.error.TweepError:
+        print("ERROR One of these statuses have been run already")
 
 print("Bot is running")
 main()
