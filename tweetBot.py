c9307fe59bfc32a5eaf36c59dc4854d1322eb633
Stefano C. Coronado <stefanocorona12@gmail.com>
2019-11-28 14:38:04 -0500

    Implemented NYTimes parser

--- tweetBot.py
+++ tweetBot.py
@@ -34,40 +34,69 @@ def main():
 
     twitter = tweet.API(auth ,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-    #formulate_npr_tweet(twitter)
+    formulate_npr_tweet(twitter)
     formulate_bbc_tweet(twitter)
+    formulate_nytimes_tweet(twitter)
     exit(0)
 
+def formulate_nytimes_tweet(api):
+    print("Attempting to send NY Times Headlines")
+    try:
+        nytimes_feed = fp.parse("https://www.nytimes.com/svc/collections/v1/publish/https://www.nytimes.com/section/world/rss.xml")
 
+        headlines = []
+        for i in range(len(nytimes_feed.entries)):
+            headlines.append(" - " + nytimes_feed.entries[i].title)
+
+        #for headline in headlines:
+            #print(headline)
+
+        low_range = 0
+        high_range = 2
+        for i in range(5):
+            message = "From the NY Times (" + str(i+1)+ "/5)\n"
+
+            for indeces in range(low_range, high_range):
+                message += headlines[indeces] + "\n"
+
+            time.sleep(5)
+            print(message)
+            api.update_status(message)
+            low_range += 2
+            high_range += 2
+
+    except tweet.error.TweepError:
+            print("An unknown error has occured")
+            exit(0)
 
 def formulate_bbc_tweet(api):
     '''This function aggregates the BBC world news feed and splits the headlines'''
+
+    print("Attempting to Send BBC Headlines")
+
     try:
 
         #TODO send 7 tweets of four headlines
         bbc_feed = fp.parse("http://feeds.bbci.co.uk/news/world/rss.xml#")
+        current_date_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M hrs")
 
 
         headlines = []
 
         for i in range(len(bbc_feed.entries)):
-           headlines.append (" -  " + bbc_feed.entries[i].title)
+           headlines.append(" -  " + bbc_feed.entries[i].title)
 
         low_range = 0
         high_range = 4
 
         for i in range (7):
 
-            if i == 0:
-                message = "From NPR News on " + current_date_time + "(" + str(i+1) + "/5)\n"
-            else:
-                message = "("+ str(i+1) + "/5)\n"
+            message = "("+ str(i+1) + "/7) BBC News\n"
 
             for indeces in range(low_range, high_range):
                 message += headlines[indeces] + "\n"
             time.sleep(5) # eliminate possibility of getting ratelimited
-            #print(len(message))
-            #api.update_status(message)
+            api.update_status(message)
             low_range += 4
             high_range += 4
 
@@ -84,7 +113,7 @@ def formulate_npr_tweet(api):
 
     '''This function aggregates the NPR RSS Feed and splits the headlines into
                 a series of five tweets'''
-
+    print("Attempting to send NPR Headlines")
 
     try:
 
